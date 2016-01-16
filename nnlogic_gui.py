e984e8df418d8067004952fb0ad4c3eb913e87a9
Nick Ivanov <nnrowan@gmail.com>
2016-01-16 20:05:22 +0200

    Text input

--- nnlogic_gui.py
+++ nnlogic_gui.py
@@ -14,14 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from tkinter import Tk, Label, Button
+from tkinter import *
 
 class NNLogicGUI:
     def __init__(self, master):
         self.master = master
         master.title("NNLogic GUI")
 
-        
+        T = Text(root, height=2, width=30)
+        T.pack()
+        T.insert(END, "Line1\nLine2\n")
 
         self.label = Label(master, text="NNLogic GUI")
         self.label.pack()
