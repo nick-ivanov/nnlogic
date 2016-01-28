f17ab0088c4bf76555894d2c8be1d2dbeb94bfbe
Nick Ivanov <nnrowan@gmail.com>
2016-01-28 21:06:22 +0200

    Converting everything into grid

--- nnlogic_gui.py
+++ nnlogic_gui.py
@@ -21,13 +21,12 @@ class NNLogicGUI:
         self.master = master
         master.title("NNLogic GUI")
 
-        frame = Frame(master)
 
-        scrollbar = Scrollbar(frame)
-        scrollbar.grid(row=0, column=1)
+        scrollbar = Scrollbar(master)
+        scrollbar.grid(row=0, column=0)
 
-        text = Text(frame, bg="blue", wrap=WORD, yscrollcommand=scrollbar.set)
-        text.grid(row=0, column=0)
+        text = Text(master, bg="blue", wrap=WORD, yscrollcommand=scrollbar.set)
+        text.grid(row=0, column=1)
 
 
 
@@ -36,21 +35,21 @@ class NNLogicGUI:
         scrollbar.config(command=text.yview)
 
         T = Text(root, height=2, width=100, bg="green")
-        T.pack()
+        T.grid(row=1, column=0)
         T.insert(END, "Line1\nLine2\nLine3")
 
         T1 = Text(root, height=2, width=100, bg="red")
-        T1.pack()
+        T1.grid(row=2, column=0)
         T1.insert(END, "AnoterLine1\nAnoterLine2\n")
 
         self.label = Label(master, text="NNLogic GUI")
-        self.label.pack()
+        self.label.grid(row=3, column=0)
 
         self.greet_button = Button(master, text="Process", command=self.greet)
-        self.greet_button.pack()
+        self.greet_button.grid(row=4, column=0)
 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        self.close_button.grid(row=5, column=0)
 
     def greet(self):
         print("Welcome to NNLogic GUI!")
