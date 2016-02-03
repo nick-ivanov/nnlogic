dc5055cb22528484460cb9140206dd4f45229a5e
Nick Ivanov <nnrowan@gmail.com>
2016-02-03 18:15:22 +0200

    Scrollbar improvements and bugfixes

--- nnlogic_gui.py
+++ nnlogic_gui.py
@@ -43,32 +43,26 @@ class NNLogicGUI:
         self.master = master
         master.title("NNLogic GUI")
 
-        scrollbar = Scrollbar(master)
-        scrollbar.grid(row=0, column=1)
+        text = ScrollText(master, width=50, height=50)
+        text.pack()
 
-        text = Text(master, bg="blue", wrap=WORD, yscrollcommand=scrollbar.set)
-        text.grid(row=0, column=0)
 
-        scrollbar.config(command=text.yview)
-
-        text.insert(END, "Line1\nLine2\nLine3")
-
-        T = Text(root, height=2, width=100, bg="green")
-        T.grid(row=1, column=0)
-        T.insert(END, "Line1\nLine2\nLine3")
-
-        T1 = Text(root, height=2, width=100, bg="red")
-        T1.grid(row=2, column=0)
-        T1.insert(END, "AnoterLine1\nAnoterLine2\n")
-
-        self.label = Label(master, text="NNLogic GUI")
-        self.label.grid(row=3, column=0)
-
-        self.greet_button = Button(master, text="Process", command=self.greet)
-        self.greet_button.grid(row=4, column=0)
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.grid(row=5, column=0)
+        # T = Text(root, height=2, width=100, bg="green")
+        # T.grid(row=1, column=0)
+        # T.insert(END, "Line1\nLine2\nLine3")
+        #
+        # T1 = Text(root, height=2, width=100, bg="red")
+        # T1.grid(row=2, column=0)
+        # T1.insert(END, "AnoterLine1\nAnoterLine2\n")
+        #
+        # self.label = Label(master, text="NNLogic GUI")
+        # self.label.grid(row=3, column=0)
+        #
+        # self.greet_button = Button(master, text="Process", command=self.greet)
+        # self.greet_button.grid(row=4, column=0)
+        #
+        # self.close_button = Button(master, text="Close", command=master.quit)
+        # self.close_button.grid(row=5, column=0)
 
     def greet(self):
         print("Welcome to NNLogic GUI!")
