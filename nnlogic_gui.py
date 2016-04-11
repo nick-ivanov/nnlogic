2e20e24a83de4276d171a3f3bbce16658702a655
Nick Ivanov <nnrowan@gmail.com>
2016-04-11 18:35:10 +0200

    More code cleanup

--- nnlogic_gui.py
+++ nnlogic_gui.py
@@ -48,23 +48,19 @@ class NNLogicGUI:
 
         self.master.config(menu=self.menubar)
 
-
         self.top_label = Label(self.master, text="NNLogic ver. 0.7", font=("Courier", 20))
         self.top_label.pack()
 
         self.output_text = ScrolledText(root, height=30, width=100, bg="#88b898")
         self.output_text.pack()
-        self.output_text.insert(END, "Line1\nLine2\nLine3")
 
         self.input_text = ScrolledText(root, height=3, width=100, bg="#aaaaff")
         self.input_text.pack()
-        #self.input_text.insert(END, "AnoterLine1\nAnoterLine2\n")
 
         self.greet_button = Button(master, text="Process", command=self.process)
         self.greet_button.pack()
 
     def process(self):
-        print("Welcome to NNLogic GUI!")
         self.output_text.delete("1.0", END)
         s = self.input_text.get("1.0", END)
         self.output_text.insert(INSERT, go2(s))
