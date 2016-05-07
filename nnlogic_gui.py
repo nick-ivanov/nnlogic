45340b84168d101d3c5c5715997d128fe2a8519e
Nick Ivanov <nnrowan@gmail.com>
2016-05-07 20:16:10 +0200

    Bugfix

--- nnlogic_gui.py
+++ nnlogic_gui.py
@@ -82,7 +82,7 @@ class NNLogicGUI:
     def paste(self):
         text = self.master.selection_get(selection='CLIPBOARD')
 
-        widget = self.focus_get()
+        widget = self.master.focus_get()
         # works for Text and Entry, at least; fails quietly
         widget.tk.call('tk_textPaste', widget._w)
         print(text)
